2b3e3e1541678654b21119b53d0e2ae5b97784fe
deedixit <deedixit@cisco.com>
2024-04-14 22:08:57 +0530

    updating app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
 from dotenv import load_dotenv
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
 from langchain_community.utilities import SQLDatabase
 import streamlit as st
 
@@ -8,6 +11,45 @@ def init_database(user: str, password: str, host: str, port: str, database: str)
     db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
     return SQLDatabase.from_uri(db_uri)
 
+def get_sql_chain(db):
+
+    #we are going to send a prompt to our language model. Prompt will be like: given this db schema, user question, and conversation history,
+    #give me a SQL query that'll respond to the user question.
+    template = """
+        You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
+        Based on the table schema below, write a SQL query that would answer the user's question. Take the conversation history into account.
+        
+        <SCHEMA>{schema}</SCHEMA>
+        
+        Conversation History: {chat_history}
+        
+        Write only the SQL query and nothing else. Do not wrap the SQL query in any other text, not even backticks.
+        
+        For example: 
+        Question: which 3 artists have the most tracks?
+        SQL Query: SELECT ArtistId, COUNT(*) as track_count FROM Track GROUP BY ArtistId ORDER BY track_count DESC LIMIT 3;
+        Question: Name 10 artists
+        SQL Query: SELECT Name FROM Artist LIMIT 10;
+        
+        Your turn:
+        
+        Question: {question}
+        SQL Query:
+    """#note the 2-shot inference
+
+    prompt = ChatPromptTemplate.from_template(template)
+    llm = ChatOpenAI(model="gpt-4-0125-preview")
+
+    def get_schema(_):
+        return db.get_table_info() #returns the schema of the db 
+
+
+
+#initialize a persistent variable for chat-history
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content="Hello! I am your SQL assistant. Ask me anything about your database.")
+    ]
 
 st.set_page_config(page_title="Chat with MySQL", page_icon=":speech_balloon:")
 st.title("Chat with MySQL")
@@ -34,4 +76,27 @@ with st.sidebar:
             st.session_state.db = db
             st.success("Connected to database!")
 
-st.chat_input("Type a message...")
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
+            st.markdown(message.content)
+    elif isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.markdown(message.content)
+
+#when user asks a question, let's add it to our chat_history
+user_query = st.chat_input("Type a message...")
+if user_query is not None and user_query != "":
+    st.session_state.chat_history.append(HumanMessage(content=user_query)) #append user_query to the chat_history
+
+    #show the user_query on the page
+    with st.chat_message("Human"):
+        st.markdown(user_query)
+
+    #if we were getting the response
+    with st.chat_message("AI"):
+        response = "I don't know"
+        st.markdown(response)
+    
+    st.session_state.chat_history.append(AIMessage(content=response)) #append AI response to the chat_history
+
