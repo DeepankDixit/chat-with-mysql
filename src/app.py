6b78539f098c529b7602a95823bb65eb377c0486
deedixit <deedixit@cisco.com>
2024-04-14 16:27:32 +0530

    updating app.py and req.txt basic

--- src/app.py
+++ src/app.py
@@ -1,8 +1,37 @@
-# pip install streamlit langchain langchain-openai langchain-groq mysql-connector-python python-dotenv
+from dotenv import load_dotenv
+from langchain_community.utilities import SQLDatabase
+import streamlit as st
 
-pip freeze | grep streamlit >> requirements.txt
-pip freeze | grep langchain >> requirements.txt
-pip freeze | grep langchain-openai >> requirements.txt
-pip freeze | grep langchain-groq >> requirements.txt
-pip freeze | grep mysql-connector-python >> requirements.txt
-pip freeze | grep python-dotenv >> requirements.txt
+load_dotenv()
+
+def init_database(user: str, password: str, host: str, port: str, database: str) -> SQLDatabase:
+    db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
+    return SQLDatabase.from_uri(db_uri)
+
+
+st.set_page_config(page_title="Chat with MySQL", page_icon=":speech_balloon:")
+st.title("Chat with MySQL")
+
+with st.sidebar:
+    st.subheader("Settings")
+    st.write("This is a simple chat application using MySQL. Connect to the database and start chatting.")
+
+    st.text_input("Host", value="localhost", key="Host") #default value set to localhost
+    st.text_input("Port", value="3306", key="Port")
+    st.text_input("User", value="root", key="User")
+    st.text_input("Password", type="password", value="admin123", key="Password")
+    st.text_input("Database", value="Chinook", key="Database")
+    
+    if st.button("Connect"):
+        with st.spinner("Connecting to database..."):
+            db = init_database(
+                st.session_state["User"],
+                st.session_state["Password"],
+                st.session_state["Host"],
+                st.session_state["Port"],
+                st.session_state["Database"]
+            )
+            st.session_state.db = db
+            st.success("Connected to database!")
+
+st.chat_input("Type a message...")
